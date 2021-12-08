14bc181d2ace59052468ea349d23bf922e7874df
Andrew Kal <andrewkalgit@gmail.com>
2021-12-07 21:39:01 -0500

    Create Disc functionality

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const button = document.querySelector('button');
+const BUTTON = document.querySelector('button');
+// const disc = document.querySelector('disc');
+const P1 = document.querySelector('#p1');
 
-button.addEventListener('click', createDisc);
+BUTTON.addEventListener('click', createDisc);
+// disc.setAttribute('draggable', true);
 
 function createDisc() {
-	const p1 = document.querySelector('#p1');
 	const disc = document.createElement('li');
-	disc.innerText = 'o';
-	p1.append(disc);
+	const COLOR1 = Math.floor(Math.random() * (255 - 25) + 25);
+	const COLOR2 = Math.floor(Math.random() * (255 - 25) + 25);
+	const COLOR3 = Math.floor(Math.random() * (255 - 25) + 25);
+	disc.style.width = `${120}px`;
+	disc.style.height = `${20}px`;
+	disc.style.border = `${1}px, solid, rgb(${COLOR1},${COLOR2},${COLOR3})`;
+	disc.style.backgroundColor = `rgb(${COLOR1},${COLOR2},${COLOR3})`;
+	disc.style.borderRadius = `${5}px`;
+	console.log(disc);
+	// Can you make an OOP model out of these? maybe later...
+	P1.append(disc);
 }
+//Search up MVC later on if you get stuck?
+
+//Array for poles
+//Objects
+
+//TO DO's
+//Find a way to decrement the variable i or width every time you make a new disc
