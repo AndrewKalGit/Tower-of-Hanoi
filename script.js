e3c9f5fc8a0774da47b84f6d2f59b41b1af76a7d
Andrew Kal <andrewkalgit@gmail.com>
2021-12-13 21:00:30 -0500

    Tower of Hanoi MVP Complete

--- script.js
+++ script.js
@@ -11,20 +11,25 @@ const WIN_CONDITION = [[], [], []];
 const emptyArray = [];
 
 let selectDisc = null;
+let selectedPole;
+let js;
 
 BUTTON.addEventListener('click', createDisc);
 Poles.addEventListener('click', function (event) {
-	let selectedPole;
 	if (event.target.parentElement.classList.contains('pole')) {
 		selectedPole = event.target.parentElement;
 	} else {
 		selectedPole = event.target;
 	}
+	if (selectDisc === null) {
+		js = DISC_ARR[selectedPole.id[1] - 1].pop();
+	}
 	if (selectDisc !== null) {
-		moveDiscOrigin(selectedPole);
+		moveDiscOrigin(selectedPole, js);
 	} else {
 		if (selectedPole.children.length > 0) {
 			selectDisc = selectedPole.children[selectedPole.children.length - 1];
+			selectDisc.style.border = '5px solid tomato';
 		}
 	}
 });
@@ -61,42 +66,56 @@ function createDisc() {
 	}
 }
 
-let positionBefore = 0;
-let positionAfter;
-function moveDiscOrigin(pole) {
-	let makeshiftIdentifierDiscs = parseInt(selectDisc.id[1]) + 1;
-	let uniqueDiscus = parseInt(pole.id[1] - 1);
-
-	if (makeshiftIdentifierDiscs === 1) {
+function moveDiscOrigin(pole, js) {
+	let jsARR = DISC_ARR[pole.id[1] - 1];
+	if (jsARR.length > 0) {
+		if (selectDisc.id[1] < jsARR[jsARR.length - 1].id[1]) {
+			pole.append(selectDisc);
+			selectDisc.style.border = 'none';
+			selectDisc = null;
+			DISC_ARR[selectedPole.id[1] - 1].push(js);
+		}
+	} else {
 		pole.append(selectDisc);
+		selectDisc.style.border = 'none';
 		selectDisc = null;
+		DISC_ARR[selectedPole.id[1] - 1].push(js);
 	}
-	if (
-		WIN_CONDITION[positionBefore].length >= WIN_CONDITION[uniqueDiscus].length
-	) {
-		WIN_CONDITION[uniqueDiscus].push(selectDisc);
-		positionAfter = uniqueDiscus;
-		if (positionBefore !== uniqueDiscus) {
-			WIN_CONDITION[positionBefore].pop();
-			positionBefore = uniqueDiscus;
-		}
-	}
-	if (makeshiftIdentifierDiscs === 2) {
-		positionAfter = uniqueDiscus;
-		if (positionAfter === uniqueDiscus){
-			WIN_CONDITION[uniqueDiscus].push(selectDisc);
-		}	if (positionBefore > uniqueDiscus) {
-			WIN_CONDITION[positionBefore].pop();
-			positionBefore = uniqueDiscus;
-		}
-	}
-	if (makeshiftIdentifierDiscs > WIN_CONDITION[positionBefore]) {
-		pole.append(selectDisc);
-	}
+	checkWin();
+
+	// let makeshiftIdentifierDiscs = parseInt(selectDisc.id[1]) + 1;
+	// let uniqueDiscus = parseInt(pole.id[1] - 1);
+
+	// if (makeshiftIdentifierDiscs === 1) {
+	// 	pole.append(selectDisc);
+	// 	selectDisc = null;
+	// }
+	// if (
+	// 	WIN_CONDITION[positionBefore].length >= WIN_CONDITION[uniqueDiscus].length
+	// ) {
+	// 	WIN_CONDITION[uniqueDiscus].push(selectDisc);
+	// 	positionAfter = uniqueDiscus;
+	// 	if (positionBefore !== uniqueDiscus) {
+	// 		WIN_CONDITION[positionBefore].pop();
+	// 		positionBefore = uniqueDiscus;
+	// 	}
+	// }
+	// if (makeshiftIdentifierDiscs === 2) {
+	// 	positionAfter = uniqueDiscus;
+	// 	if (positionAfter === uniqueDiscus){
+	// 		WIN_CONDITION[uniqueDiscus].push(selectDisc);
+	// 	}	if (positionBefore > uniqueDiscus) {
+	// 		WIN_CONDITION[positionBefore].pop();
+	// 		positionBefore = uniqueDiscus;
+	// 	}
+	// }
+	// if (makeshiftIdentifierDiscs > WIN_CONDITION[positionBefore] && selectDisc !== null) {
+	// 	pole.append(selectDisc);
+	// }
 }
 
 function checkWin() {
-	if (DISC_ARR[0] === WIN_CONDITION[2]) {
+	if (DISC_ARR[2].length === 3) {
 		alert('Completed!');
 	}
 }
